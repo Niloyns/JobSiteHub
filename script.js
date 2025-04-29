c7edd306f1c1b5adea17a94655cd9862f973b495
Niloyns <niloysarkar1998@gmail.com>
2025-04-30 01:57:17 +0530

    update

--- script.js
+++ script.js
@@ -1,12 +1,20 @@
 document.addEventListener("DOMContentLoaded", () => {
   const jobSites = [
-    { name: "LinkedIn", url: "https://www.linkedin.com/login" },
-    { name: "Naukri", url: "https://www.naukri.com/mnjuser/login" },
-    { name: "Indeed", url: "https://secure.indeed.com/account/login" },
-    {
-      name: "Monster",
-      url: "https://www.monsterindia.com/seeker/registration",
-    },
+    { name: "LinkedIn India", url: "https://in.linkedin.com/" },
+    { name: "Naukri", url: "https://www.naukri.com/" },
+    { name: "Indeed India", url: "https://www.indeed.co.in/" },
+    { name: "Monster India", url: "https://www.monsterindia.com/" },
+    { name: "Shine", url: "https://www.shine.com/" },
+    { name: "Glassdoor India", url: "https://www.glassdoor.co.in/index.htm" },
+    { name: "TimesJobs", url: "https://www.timesjobs.com/" },
+    { name: "Freshersworld", url: "https://www.freshersworld.com/" },
+    { name: "Internshala", url: "https://internshala.com/" },
+    { name: "Hirist", url: "https://www.hirist.com/" },
+    { name: "CutShort", url: "https://cutshort.io/" },
+    { name: "UpWork India", url: "https://www.upwork.com/" },
+    { name: "AngelList Talent India", url: "https://angel.co/" },
+    { name: "Jobhai", url: "https://www.jobhai.com/" },
+    { name: "JobStreet India", url: "https://www.jobstreet.co.in/" },
   ];
 
   const launchBtn = document.getElementById("button");
